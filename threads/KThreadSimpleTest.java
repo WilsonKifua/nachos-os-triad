aebe7040946dd23d7792f20a705a26f38aa9f688
kwanh05 <kwanh05@e52bff7a-ce61-11de-8d2e-8bf2cb4fc7ae>
2009-11-12 23:38:21 +0000

    *modified KThread.java for Question#3 *added debug lines and fixed comments in KThreadSimpleTest.java for Questions#2

--- threads/KThreadSimpleTest.java
+++ threads/KThreadSimpleTest.java
@@ -14,26 +14,32 @@ public class KThreadSimpleTest {
 
 	System.out.println("**** Simple KThread testing begins ****");
 	
-	//Hello World Thread 1
+	//Hello World 1
     Runnable HelloWorld1 = new Runnable() {
 		public void run() {
 	        System.out.println("Hello World!");
 		}
 	};
+	
+	//Hello World Thread 1 and run
     KThread newThread1 = new KThread(HelloWorld1);
     newThread1.setName("HelloWorldThread-1");
+    Lib.debug('t',"Thread:" + newThread1.toString());
     HelloWorld1.run();
-    
-    //Hello World Thread 2
+	
+    //Hello World 2
     Runnable HelloWorld2 = new Runnable() {
 		public void run() {
 	        System.out.println("Hello World!");
 		}
 	};
+ 
+    //Hello World Thread 2 and run
     KThread newThread2 = new KThread(HelloWorld2);
     newThread2.setName("HelloWorldThread-2");
+    Lib.debug('t',"Thread:" + newThread2.toString());
     HelloWorld2.run();
-	
+
 	KThread.yield();
 	System.out.println("**** Simple KThread testing ends ****");
     }
