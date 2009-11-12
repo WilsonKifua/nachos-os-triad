65b9d0f924414f57e245a00c7fd86574b611b607
kwanh05 <kwanh05@e52bff7a-ce61-11de-8d2e-8bf2cb4fc7ae>
2009-11-12 07:40:16 +0000

    Uncommented some lines of selfTest() in ThreadedKernel.java so KThreadSimpleTest, AlarmTest, etc will work. Also, wrote KThreadSimpleTest for Question#1.

--- trunk/threads/KThreadSimpleTest.java
+++ trunk/threads/KThreadSimpleTest.java
@@ -13,10 +13,28 @@ public class KThreadSimpleTest {
     public static void runTest() {
 
 	System.out.println("**** Simple KThread testing begins ****");
-
-        
+	
+	//Hello World Thread 1
+    Runnable HelloWorld1 = new Runnable() {
+		public void run() {
+	        System.out.println("Hello World!");
+		}
+	};
+    KThread newThread1 = new KThread(HelloWorld1);
+    newThread1.setName("HelloWorldThread-1");
+    HelloWorld1.run();
+    
+    //Hello World Thread 2
+    Runnable HelloWorld2 = new Runnable() {
+		public void run() {
+	        System.out.println("Hello World!");
+		}
+	};
+    KThread newThread2 = new KThread(HelloWorld2);
+    newThread2.setName("HelloWorldThread-2");
+    HelloWorld2.run();
+	
 	KThread.yield();
 	System.out.println("**** Simple KThread testing ends ****");
     }
-
 }
