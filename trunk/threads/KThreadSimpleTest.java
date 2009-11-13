ead39c25f8812f63aca255eb70725c4c28b02ab0
kwanh05 <kwanh05@e52bff7a-ce61-11de-8d2e-8bf2cb4fc7ae>
2009-11-13 09:07:10 +0000

    *KThreadSimpleTest.java updated, changed everything to to how it should be according to KThread.java *running nachos with the '-d t' option will show correct debug output

--- trunk/threads/KThreadSimpleTest.java
+++ trunk/threads/KThreadSimpleTest.java
@@ -7,40 +7,39 @@ import nachos.machine.*;
  */
 public class KThreadSimpleTest {
 
-    /**
-     * Tests whether this module is working.
-     */
-    public static void runTest() {
+  /**
+   * Tests whether this module is working.
+   */
+  public static void runTest() {
 
-	System.out.println("**** Simple KThread testing begins ****");
-	
-	//Hello World 1
-    Runnable HelloWorld1 = new Runnable() {
-		public void run() {
-	        System.out.println("Hello World!");
-		}
-	};
-	
-	//Hello World Thread 1 and run
-    KThread newThread1 = new KThread(HelloWorld1);
-    newThread1.setName("HelloWorldThread-1");
-    Lib.debug('t',"Thread:" + newThread1.toString());
-    HelloWorld1.run();
-	
-    //Hello World 2
-    Runnable HelloWorld2 = new Runnable() {
-		public void run() {
-	        System.out.println("Hello World!");
-		}
-	};
- 
-    //Hello World Thread 2 and run
-    KThread newThread2 = new KThread(HelloWorld2);
-    newThread2.setName("HelloWorldThread-2");
-    Lib.debug('t',"Thread:" + newThread2.toString());
-    HelloWorld2.run();
+    System.out.println("**** Simple KThread testing begins ****");
 
-	KThread.yield();
-	System.out.println("**** Simple KThread testing ends ****");
+  /**
+   * Class for HelloWorld implements Runnable
+   */
+    class HelloWorld implements Runnable {
+      public void run() {
+        System.out.println("Hello World!");
+      }
     }
+    
+    //create runnable HelloWorld objects
+    HelloWorld hw1 = new HelloWorld();
+    HelloWorld hw2 = new HelloWorld();
+    
+    //create threads
+    KThread newThread1 = new KThread(hw1);
+    KThread newThread2 = new KThread(hw2);
+    
+    //set thread names
+    newThread1.setName("HelloWorldThread-1");
+    newThread2.setName("HelloWorldThread-2");
+
+    //start running
+    newThread1.fork();
+    newThread2.fork();
+    
+    KThread.yield();
+    System.out.println("**** Simple KThread testing ends ****")
+  }
 }
