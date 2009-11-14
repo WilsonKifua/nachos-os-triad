3e7fd9dc70106de53310dcb7feae1efa3bc4cfad
john.km.zhou <john.km.zhou@e52bff7a-ce61-11de-8d2e-8bf2cb4fc7ae>
2009-11-14 03:29:44 +0000

    Implemented condition variables in Condition2.java. Formatted code.

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -3,66 +3,78 @@ package nachos.threads;
 import nachos.machine.*;
 
 /**
- * An implementation of condition variables that disables interrupt()s for
- * synchronization.
- *
+ * An implementation of condition variables that disables interrupt()s for synchronization.
+ * 
  * <p>
  * You must implement this.
- *
- * @see	nachos.threads.Condition
+ * 
+ * @see nachos.threads.Condition
  */
 public class Condition2 {
-    /**
-     * Allocate a new condition variable.
-     *
-     * @param	conditionLock	the lock associated with this condition
-     *				variable. The current thread must hold this
-     *				lock whenever it uses <tt>sleep()</tt>,
-     *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
-     */
-    public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
+  private static final char dbgCondition = 'c';
+  private Lock conditionLock;
+  private ThreadQueue waitQueue = ThreadedKernel.scheduler.newThreadQueue(false);
+
+  /**
+   * Allocate a new condition variable.
+   * 
+   * @param conditionLock the lock associated with this condition variable. The current thread must
+   * hold this lock whenever it uses <tt>sleep()</tt>, <tt>wake()</tt>, or <tt>wakeAll()</tt>.
+   */
+  public Condition2(Lock conditionLock) {
+    this.conditionLock = conditionLock;
+  }
+
+  /**
+   * Atomically release the associated lock and go to sleep on this condition variable until another
+   * thread wakes it using <tt>wake()</tt>. The current thread must hold the associated lock. The
+   * thread will automatically reacquire the lock before <tt>sleep()</tt> returns.
+   */
+  public void sleep() {
+    /* If the current thread doesn't hold the lock, then abort */
+    Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+    boolean status = Machine.interrupt().disable();
+    conditionLock.release();
+    waitQueue.waitForAccess(KThread.currentThread());
+    KThread.sleep();
+    // acquires lock after being woke up
+    conditionLock.acquire();
+    Machine.interrupt().restore(status);
+
+  }
+
+  /**
+   * Wake up at most one thread sleeping on this condition variable. The current thread must hold
+   * the associated lock.
+   */
+  public void wake() {
+    Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+    boolean status = Machine.interrupt().disable();
+    KThread thread = waitQueue.nextThread();
+    if (thread != null) {
+      thread.ready();
     }
+    Machine.interrupt().restore(status);
 
-    /**
-     * Atomically release the associated lock and go to sleep on this condition
-     * variable until another thread wakes it using <tt>wake()</tt>. The
-     * current thread must hold the associated lock. The thread will
-     * automatically reacquire the lock before <tt>sleep()</tt> returns.
-     */
-    public void sleep() {
+  }
 
-        /* If the current thread doesn't hold the lock, then abort */
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
+  /**
+   * Wake up all threads sleeping on this condition variable. The current thread must hold the
+   * associated lock.
+   */
+  public void wakeAll() {
+    Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+    boolean status = Machine.interrupt().disable();
+    for(KThread thread = waitQueue.nextThread(); thread != null; thread = waitQueue.nextThread()){
+      thread.ready();
     }
+    Machine.interrupt().restore(status);
+  }
 
-    /**
-     * Wake up at most one thread sleeping on this condition variable. The
-     * current thread must hold the associated lock.
-     */
-    public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
-    }
-
-    /**
-     * Wake up all threads sleeping on this condition variable. The current
-     * thread must hold the associated lock.
-     */
-    public void wakeAll() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
-
-    }
-
-    /**
-     * Tests whether this module is working.
-     */
-    public static void selfTest() {
-        Condition2Test.runTest();
-    }
-
-    private static final char dbgCondition = 'c';
-
-    private Lock conditionLock;
+  /**
+   * Tests whether this module is working.
+   */
+  public static void selfTest() {
+    Condition2Test.runTest();
+  }
 }
--- threads/ThreadedKernel.java
+++ threads/ThreadedKernel.java
@@ -6,85 +6,79 @@ import nachos.machine.*;
  * A multi-threaded OS kernel.
  */
 public class ThreadedKernel extends Kernel {
-    /**
-     * Allocate a new multi-threaded kernel.
-     */
-    public ThreadedKernel() {
-	super();
-    }
+  /**
+   * Allocate a new multi-threaded kernel.
+   */
+  public ThreadedKernel() {
+    super();
+  }
 
-    /**
-     * Initialize this kernel. Creates a scheduler, the first thread, and an
-     * alarm, and enables interrupts. Creates a file system if necessary.   
-     */
-    public void initialize(String[] args) {
-	// set scheduler
-	String schedulerName = Config.getString("ThreadedKernel.scheduler");
-	scheduler = (Scheduler) Lib.constructObject(schedulerName);
+  /**
+   * Initialize this kernel. Creates a scheduler, the first thread, and an alarm, and enables
+   * interrupts. Creates a file system if necessary.
+   */
+  public void initialize(String[] args) {
+    // set scheduler
+    String schedulerName = Config.getString("ThreadedKernel.scheduler");
+    scheduler = (Scheduler) Lib.constructObject(schedulerName);
 
-	// set fileSystem
-	String fileSystemName = Config.getString("ThreadedKernel.fileSystem");
-	if (fileSystemName != null)
-	    fileSystem = (FileSystem) Lib.constructObject(fileSystemName);
-	else if (Machine.stubFileSystem() != null)
-	    fileSystem = Machine.stubFileSystem();
-	else
-	    fileSystem = null;
+    // set fileSystem
+    String fileSystemName = Config.getString("ThreadedKernel.fileSystem");
+    if (fileSystemName != null)
+      fileSystem = (FileSystem) Lib.constructObject(fileSystemName);
+    else if (Machine.stubFileSystem() != null)
+      fileSystem = Machine.stubFileSystem();
+    else
+      fileSystem = null;
 
-	// start threading
-	new KThread(null);
+    // start threading
+    new KThread(null);
 
-	alarm  = new Alarm();
+    alarm = new Alarm();
 
-	Machine.interrupt().enable();
-    }
+    Machine.interrupt().enable();
+  }
 
-    /**
-     * Test this kernel. Test various classes. 
-     */	
-    public void selfTest() {
-        System.out.println("\n*** Nachos Kernel Successfull Started ***\n");
-//<<<<<<< .mine
-	//KThread.simpleSelfTest();
-	KThread.selfTest();
-//=======
-	KThread.simpleSelfTest();
-	KThread.selfTest();
-//>>>>>>> .r3
-	//Semaphore.selfTest();
-        //Condition.selfTest();
-        //Condition2.selfTest();
-	Alarm.selfTest();
-	//Communicator.selfTest();
-    }
-    
-    /**
-     * A threaded kernel does not run user programs, so this method does
-     * nothing.
-     */
-    public void run() {
-    }
+  /**
+   * Test this kernel. Test various classes.
+   */
+  public void selfTest() {
+    System.out.println("\n*** Nachos Kernel Successfull Started ***\n");
+    // KThread.simpleSelfTest();
+    // KThread.selfTest();
+    // Semaphore.selfTest();
+    // Condition.selfTest();
+    // Condition2.selfTest();
+    // Alarm.selfTest();
+    // Communicator.selfTest();
+  }
 
-    /**
-     * Terminate this kernel. Never returns.
-     */
-    public void terminate() {
-	Machine.halt();
-    }
+  /**
+   * A threaded kernel does not run user programs, so this method does nothing.
+   */
+  public void run() {
+  }
 
-    /** Globally accessible reference to the scheduler. */
-    public static Scheduler scheduler = null;
-    /** Globally accessible reference to the alarm. */
-    public static Alarm alarm = null;
-    /** Globally accessible reference to the file system. */
-    public static FileSystem fileSystem = null;
+  /**
+   * Terminate this kernel. Never returns.
+   */
+  public void terminate() {
+    Machine.halt();
+  }
 
-    // dummy variables to make javac smarter
-    private static RoundRobinScheduler dummy1 = null;
-    private static PriorityScheduler dummy2 = null;
-    private static LotteryScheduler dummy3 = null;
-    private static Condition2 dummy4 = null;
-    private static Communicator dummy5 = null;
-    private static Rider dummy6 = null;
-    private static ElevatorController dummy7 = null;
+  /** Globally accessible reference to the scheduler. */
+  public static Scheduler scheduler = null;
+  /** Globally accessible reference to the alarm. */
+  public static Alarm alarm = null;
+  /** Globally accessible reference to the file system. */
+  public static FileSystem fileSystem = null;
+
+  // dummy variables to make javac smarter
+  private static RoundRobinScheduler dummy1 = null;
+  private static PriorityScheduler dummy2 = null;
+  private static LotteryScheduler dummy3 = null;
+  private static Condition2 dummy4 = null;
+  private static Communicator dummy5 = null;
+  private static Rider dummy6 = null;
+  private static ElevatorController dummy7 = null;
 }
