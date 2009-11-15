6066ff9b5e24f21d86b277685e9be40381594b0c
kwanh05 <kwanh05@e52bff7a-ce61-11de-8d2e-8bf2cb4fc7ae>
2009-11-15 10:20:24 +0000

    *Implemented Alarm.java and AlarmTest.java **not entirely sure if they are correct *fixed ThreadedKernel.java

--- threads/Alarm.java
+++ threads/Alarm.java
@@ -7,74 +7,75 @@ import nachos.machine.*;
  * until a certain time.
  */
 public class Alarm {
-    /**
-     * Allocate a new Alarm. Set the machine's timer interrupt handler to this
-     * alarm's callback.
-     *
-     * <p><b>Note</b>: Nachos will not function correctly with more than one
-     * alarm.
-     */
-    public Alarm() {
+ /**
+  * Allocate a new Alarm. Set the machine's timer interrupt handler to this
+  * alarm's callback.
+  *
+  * <p><b>Note</b>: Nachos will not function correctly with more than one
+  * alarm.
+  */
+  Lock mutex = new Lock();
+  Condition cond = new Condition(mutex);
+  long wakeTime;
+  
+  public Alarm() {
 	Machine.timer().setInterruptHandler(new Runnable() {
 		public void run() { timerInterrupt(); }
-	    });
-    }
+	  });
+  }
 
-    /**
-     * The timer interrupt handler. This is called by the machine's timer
-     * periodically (approximately every 500 clock ticks). Causes the current
-     * thread to yield, forcing a context switch if there is another thread
-     * that should be run.
-     */
-    public void timerInterrupt() {
+  /**
+   * The timer interrupt handler. This is called by the machine's timer
+   * periodically (approximately every 500 clock ticks). Causes the current
+   * thread to yield, forcing a context switch if there is another thread
+   * that should be run.
+   */
+  public void timerInterrupt() {
 	Lib.debug(dbgAlarm,"In Interrupt Handler (time = "+Machine.timer().getTime()+")");
-	Lock object = new Lock();
-	object.acquire();
-	KThread.currentThread().yield();
-	object.release();
-    }
+	mutex.acquire();  //mutex lock
+	if(wakeTime<=Machine.timer().getTime()){  //test if time to wake
+	  cond.wake();  //cond wake
+  }
+	mutex.release();  //mutex unlock
+  KThread.currentThread().yield();  //yield
+  }
 
-    /**
-     * Put the current thread to sleep for at least <i>x</i> ticks,
-     * waking it up in the timer interrupt handler. The thread must be
-     * woken up (placed in the scheduler ready set) during the first timer
-     * interrupt where
-     *
-     * <p><blockquote>
-     * (current time) >= (WaitUntil called time)+(x)
-     * </blockquote>
-     *
-     * @param	x	the minimum number of clock ticks to wait.
-     *
-     * @see	nachos.machine.Timer#getTime()
-     */
-    public void waitUntil(long x) {
+  /**
+   * Put the current thread to sleep for at least <i>x</i> ticks,
+   * waking it up in the timer interrupt handler. The thread must be
+   * woken up (placed in the scheduler ready set) during the first timer
+   * interrupt where
+   *
+   * <p><blockquote>
+   * (current time) >= (WaitUntil called time)+(x)
+   * </blockquote>
+   *
+   * @param	x	the minimum number of clock ticks to wait.
+   *
+   * @see	nachos.machine.Timer#getTime()
+   */
+  public void waitUntil(long x) {
 	// This is a bad busy waiting solution 
-	// long wakeTime = Machine.timer().getTime() + x;
-	// while (wakeTime > Machine.timer().getTime())
-	//    KThread.yield();
-      Lock object = new Lock();
-      object.acquire();
-      System.out.println(object.isHeldByCurrentThread());
-      long wakeTime = Machine.timer().getTime() + x;
-      System.out.println(Machine.timer().getTime());
-      System.out.println(wakeTime);
-      Alarm obj = new Alarm();
-      while (Machine.timer().getTime() <= wakeTime ) {
-    	System.out.println(Machine.timer().getTime());  
-        obj.timerInterrupt();
-      }
-      object.release();
-      System.out.println("Finished");
-    }
+//	 long wakeTime = Machine.timer().getTime() + x;
+//	 while (wakeTime > Machine.timer().getTime())
+//	  KThread.yield();
 
-    /**
-     * Tests whether this module is working.
-     */
-    public static void selfTest() {
+    mutex.acquire();  //mutex lock
+    wakeTime = Machine.timer().getTime() + x; //calc wake time
+    Lib.debug(dbgAlarm,"In Wait Until (wakeTime = "+wakeTime+")");
+    if(wakeTime > Machine.timer().getTime()){ //if not wake time 
+      cond.sleep(); //cond sleep, wait wake from timerInterrupt
+      Lib.debug(dbgAlarm,"In Wake Up: (wakeTime = "+wakeTime+")");
+    }
+    mutex.release();  //mutex unlock
+  }
+
+  /**
+   * Tests whether this module is working.
+   */
+  public static void selfTest() {
 	AlarmTest.runTest();
-    }
+  }
 
-    private static final char dbgAlarm = 'a';
-
+  private static final char dbgAlarm = 'a';
 }
--- threads/AlarmTest.java
+++ threads/AlarmTest.java
@@ -1,39 +1,54 @@
 package nachos.threads;
 
 import nachos.machine.*;
-import java.util.Random;
-
 
 /**
  * A Tester for the Alarm class
  */
 public class AlarmTest {
 
+    private static class aTest implements Runnable {
+        public void run() {
+             System.out.println("Alarm Ringing! (time = "
+             +Machine.timer().getTime()+")");
+        }
+    }
+
     public static void runTest() {
-	System.out.println("**** Alarm testing begins ****");
-	Runnable wait1 = new Runnable() {
-	  	public void run() {
-	  		Alarm obj = new Alarm();
-	  		obj.waitUntil(1000);
-	  		System.out.println("OK");
-	  		System.out.println(obj.toString());
-	  	}	
-	};
-	KThread newThread1 = new KThread(wait1);
-	newThread1.setName("WaitingThread");
-	Runnable wait2 = new Runnable() {
-		public void run() {
-			Alarm obj2 = new Alarm();
-			obj2.waitUntil(20);
-			System.out.println("OK2");
-			System.out.println(obj2.toString());
-		}
-	};
-	KThread newThread2 = new KThread(wait2);
-	newThread2.setName("WaitingThread2");
-	wait1.run();
-	wait2.run();
-	System.out.println("**** Alarm testing end ****");
+	    System.out.println("**** Alarm testing begins ****");
+
+      //wait intervals
+      long wTime1=900000;
+      long wTime2=1000000;
+      long wTime3=600000;
+
+
+      //create alarm and test objects
+      Alarm aRun = new Alarm();
+      aTest aTest1 = new aTest();
+      aTest aTest2 = new aTest();
+      aTest aTest3 = new aTest();
+      
+      //create threads for each aTest object
+      KThread aThread1 = new KThread( aTest1 );
+      KThread aThread2 = new KThread( aTest2 );
+      KThread aThread3 = new KThread( aTest3 );
+      
+      //name each thread
+      aThread1.setName( "aThread-1" );
+      aThread2.setName( "aThread-2" );
+      aThread3.setName( "aThread-3" );
+
+      //run threads with alarms      
+      aRun.waitUntil(wTime1);
+      aThread2.fork();
+      aRun.waitUntil(wTime2);
+      aThread1.fork();
+      aRun.waitUntil(wTime3);
+      aThread3.fork();
+      
+  	  KThread.yield();
+    	System.out.println("**** Alarm testing end ****");
     }
 
 }
